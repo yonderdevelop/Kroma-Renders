7ad50a1b0630681d2ba1eb78136edc3d530ea08a
yonderdevelop <yonder.dev@protonmail.com>
2026-08-08 17:19:29 -0400

    English as default language

--- src/context/LanguageContext.jsx
+++ src/context/LanguageContext.jsx
@@ -5,7 +5,7 @@ import { siteData as siteDataEn } from '../data/siteData.en';
 const LanguageContext = createContext(null);
 
 export function LanguageProvider({ children }) {
-  const [lang, setLang] = useState('es');
+  const [lang, setLang] = useState('en');
 
   useEffect(() => {
     document.documentElement.lang = lang;
